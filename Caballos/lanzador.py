1d2e825dd30f90119a47a88a9bb9e69cbde357e5
Tomas-Dorado <tdoradog@student.universidadunie.com>
2025-04-08 10:41:05 +0000

    se añadio los numeros a calcular

--- Caballos/lanzador.py
+++ Caballos/lanzador.py
@@ -5,7 +5,7 @@ import sys
 def lanzador():
     caballo = CaballoAjedrez()
     print("Cantidad de movimientos | Posibilidades válidas")
-    for movimientos in [1, 2, 32]:
+    for movimientos in [1, 2,3,5,8,10,15,18,21,23, 32]:
         print(f"{movimientos:23} | {caballo.calcular_movimientos_totales(movimientos)}")
 
 lanzador()
