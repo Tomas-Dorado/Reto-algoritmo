da4436608a17cddbffad8965e8aece9b70b4be8f
Tomas-Dorado <tdoradog@student.universidadunie.com>
2025-04-08 09:52:31 +0000

    se creo el menu

---
+++ menu.py
@@ -0,0 +1,26 @@
+import subprocess
+import os
+
+
+
+def menu():
+    while True:
+        print("\nMenú de Ejercicios")
+        print("1. Ejercicio 1")
+        print("2. Ejercicio 2")
+        print("3. Salir")
+        opcion = input("Selecciona una opción: ")
+
+        if opcion == "1":
+            print("Ejecutando el Ejercicio 1...")
+            ruta = os.path.join("Caballos", "lanzador.py")
+            subprocess.run(["python", ruta])
+        elif opcion == "2":
+            print("Ejecutando el Ejercicio 2...")
+            ruta = os.path.join("lanzador", "lanzador.py")
+            subprocess.run(["python", ruta])
+        elif opcion == "3":
+            print("Saliendo del programa...")
+            break
+        else:
+            print("Opción no válida. Intenta de nuevo.")
